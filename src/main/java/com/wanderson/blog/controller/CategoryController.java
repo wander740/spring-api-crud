2afcf7fff8c87536bca443ec6f733d557ce34982
Wanderson Ramos <wandersonramos740@gmail.com>
2023-10-03 00:07:26 -0300

    feat: create CategoryController

---
+++ src/main/java/com/wanderson/blog/controller/CategoryController.java
@@ -0,0 +1,25 @@
+package com.wanderson.blog.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wanderson.blog.model.Category;
+import com.wanderson.blog.service.CategoryService;
+
+@RestController
+@RequestMapping("/api/category")
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService){
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public List<Category> list(){
+        return categoryService.list();
+    }
+}
