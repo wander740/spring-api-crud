5121e505a55073bccabc88153598df735a0d26ad
Wanderson Ramos <wandersonramos740@gmail.com>
2023-10-02 21:30:28 -0300

    feat: create NewsService

---
+++ src/main/java/com/wanderson/blog/service/NewsService.java
@@ -0,0 +1,23 @@
+package com.wanderson.blog.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.wanderson.blog.model.News;
+import com.wanderson.blog.repository.NewsRepository;
+
+@Service
+public class NewsService {
+    
+    
+    private final NewsRepository newsRepository;
+
+    public NewsService(NewsRepository newsRepository){
+        this.newsRepository = newsRepository;
+    }
+
+    public List<News> list(){
+        return newsRepository.findAll();
+    }
+}
