4e87a9453045f3f65d5ba8fc7aa98591d0dc39f3
Wanderson Ramos <wandersonramos740@gmail.com>
2023-10-02 21:30:43 -0300

    feat: create NewsController

---
+++ src/main/java/com/wanderson/blog/controller/NewsController.java
@@ -0,0 +1,26 @@
+package com.wanderson.blog.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wanderson.blog.model.News;
+import com.wanderson.blog.service.NewsService;
+
+@RestController
+@RequestMapping("/api/news")
+public class NewsController {
+    
+    private final NewsService newsService;
+
+    public NewsController(NewsService newsService){
+        this.newsService = newsService;
+    }
+
+    @GetMapping
+    public List<News> list(){
+        return newsService.list();
+    }
+}
