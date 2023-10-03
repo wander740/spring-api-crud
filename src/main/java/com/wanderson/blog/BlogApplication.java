9de1af028272140808a8aad53adfc618eb363545
Wanderson Ramos <wandersonramos740@gmail.com>
2023-10-02 21:25:34 -0300

    feat: first commit

---
+++ src/main/java/com/wanderson/blog/BlogApplication.java
@@ -0,0 +1,44 @@
+package com.wanderson.blog;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.wanderson.blog.model.Category;
+import com.wanderson.blog.model.News;
+import com.wanderson.blog.repository.CategoryRepository;
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDatabase(CategoryRepository categoryRepository){
+		return args -> {
+			categoryRepository.deleteAll();
+
+			Category category = new Category();
+			category.setTitle("Sports");
+
+			News c = new News();
+			c.setTitle("Teste um");
+			c.setText("adfasdfsadfsadfsadf");
+			c.setCategory(category);
+
+			category.getNews().add(c);
+			
+			News c1 = new News();
+			c1.setTitle("Teste dois");
+			c1.setText("adfasdfsadfsafasdfaadfsadf");
+			c1.setCategory(category);
+
+			category.getNews().add(c1);
+
+			categoryRepository.save(category);
+		};
+	}
+}
