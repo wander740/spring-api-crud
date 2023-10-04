9f90cc85b53879237125924391287b5530f7353c
Wanderson Ramos <wandersonramos740@gmail.com>
2023-10-03 22:53:20 -0300

    feat: add ResponseStatus

--- src/main/java/com/wanderson/blog/controller/CategoryController.java
+++ src/main/java/com/wanderson/blog/controller/CategoryController.java
@@ -2,10 +2,12 @@ package com.wanderson.blog.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wanderson.blog.model.Category;
@@ -28,6 +30,7 @@ public class CategoryController {
     }
 
     @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
     public Category create(@RequestBody Category category){
         return categoryService.create(category);
     }
--- src/main/java/com/wanderson/blog/controller/NewsController.java
+++ src/main/java/com/wanderson/blog/controller/NewsController.java
@@ -2,10 +2,12 @@ package com.wanderson.blog.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wanderson.blog.dto.NewsDTO;
@@ -36,6 +38,7 @@ public class NewsController {
     }
 
     @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
     public NewsDTO create(@RequestBody NewsDTO newsDTO) {
         return newsService.create(newsDTO);
     }
@@ -46,6 +49,7 @@ public class NewsController {
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
         newsService.delete(id);
     }
