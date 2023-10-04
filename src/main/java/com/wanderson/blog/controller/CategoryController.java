e5a25b8e15fa93961753269a9e0eebd867ef62e3
Wanderson Ramos <wandersonramos740@gmail.com>
2023-10-04 19:17:16 +0000

    feat: add findById

--- src/main/java/com/wanderson/blog/controller/CategoryController.java
+++ src/main/java/com/wanderson/blog/controller/CategoryController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,8 @@ import com.wanderson.blog.model.Category;
 import com.wanderson.blog.service.CategoryService;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 @Valid
 @RestController
@@ -30,6 +33,11 @@ public class CategoryController {
     public List<Category> list() {
         return categoryService.list();
     }
+
+    @GetMapping("/{id}")
+    public Category findById(@PathVariable @NotNull @Positive Long id){
+        return categoryService.findById(id);
+    } 
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
