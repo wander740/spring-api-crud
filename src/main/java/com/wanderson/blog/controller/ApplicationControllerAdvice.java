d6ad87cbe166a9143157752ceeca381bcef549dc
Wanderson Ramos <wandersonramos740@gmail.com>
2023-10-04 22:18:58 -0300

    feat: add new exception message

--- src/main/java/com/wanderson/blog/controller/ApplicationControllerAdvice.java
+++ src/main/java/com/wanderson/blog/controller/ApplicationControllerAdvice.java
@@ -6,8 +6,11 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import com.wanderson.blog.exception.RecordNotFoundException;
+
+import jakarta.validation.ConstraintViolationException;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
@@ -24,17 +27,31 @@ public class ApplicationControllerAdvice {
         return ex.getBindingResult().getFieldErrors().stream()
             .map(error -> error.getField() + " " + error.getDefaultMessage())
             .reduce("", (acc, error) -> acc + error + "\n");
-    } 
+    }
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleConstraintViolationException(ConstraintViolationException ex){
+        return ex.getConstraintViolations().stream()
+            .map(error -> error.getPropertyPath() + " " + error.getMessage())
+            .reduce("", (acc, error) -> acc + error + "\n");
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex){
+        if(ex != null && ex.getRequiredType() != null){
+            String type = ex.getRequiredType().getName();
+            String[] typeParts = type.split("\\.");
+            String typeName = typeParts[typeParts.length -1];
+            return ex.getName() + " should be of type " + typeName;
+        }
+        return "Argument type not valid";
+    } 
+    
     @ExceptionHandler(HttpMessageNotReadableException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleHttpMessageNotReadableException(HttpMessageNotReadableException ex){
-        return ex.getMessage();
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleIllegalArgumentException(IllegalArgumentException ex){
-        return ex.getMessage();
+        return "Houve um problema com a requisição. Verifique os dados enviados.";
     }
 }
