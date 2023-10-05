7150972fd12ec6bdb2ae9292789b882e8de14e03
Wanderson Ramos <wandersonramos740@gmail.com>
2023-10-04 23:02:34 -0300

    fix: query

--- src/main/java/com/wanderson/blog/repository/NewsRepository.java
+++ src/main/java/com/wanderson/blog/repository/NewsRepository.java
@@ -10,6 +10,6 @@ import com.wanderson.blog.model.News;
 
 public interface NewsRepository extends JpaRepository<News, Long>{
 
-    @Query("SELECT ne.id, ne.title, ne.text FROM Category AS ca, News AS ne WHERE ca.id = ne.category.id AND (ca.title LIKE %:title% OR ne.title LIKE %:title%)")
+    @Query("SELECT ne FROM Category AS ca, com.wanderson.blog.model.News AS ne WHERE ca.id = ne.category.id AND (ca.title LIKE %:title% OR ne.title LIKE %:title%)")
     List<News> findByTitleContaining(@Param("title") String title);
 }
