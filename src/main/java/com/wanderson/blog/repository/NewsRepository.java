94020609013534149c6300656603cfce9da488af
Wanderson Ramos <wandersonramos740@gmail.com>
2023-10-05 23:11:51 -0300

    feat: add postgresql

--- src/main/java/com/wanderson/blog/repository/NewsRepository.java
+++ src/main/java/com/wanderson/blog/repository/NewsRepository.java
@@ -10,6 +10,9 @@ import com.wanderson.blog.model.News;
 
 public interface NewsRepository extends JpaRepository<News, Long> {
 
-    @Query("SELECT ne FROM Category AS ca, com.wanderson.blog.model.News AS ne WHERE ca.id = ne.category.id AND (ca.title LIKE %:title% OR ne.title LIKE %:title%)")
+    @Query("SELECT ne \n" + //
+            "FROM com.wanderson.blog.model.News AS ne \n" + //
+            "JOIN ne.category AS ca \n" + //
+            "WHERE (ca.title LIKE %:title% OR ne.title LIKE %:title%)")
     List<News> findByTitleContaining(@Param("title") String title);
 }
