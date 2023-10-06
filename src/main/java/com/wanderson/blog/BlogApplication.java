898424602366e22b527d03e025683f24a1ff9e70
Wanderson Ramos <wandersonramos740@gmail.com>
2023-10-05 23:17:13 -0300

    refactor: delete bean initDatabase

--- src/main/java/com/wanderson/blog/BlogApplication.java
+++ src/main/java/com/wanderson/blog/BlogApplication.java
@@ -1,14 +1,7 @@
 package com.wanderson.blog;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.wanderson.blog.model.Category;
-import com.wanderson.blog.model.News;
-import com.wanderson.blog.repository.CategoryRepository;
-
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 
