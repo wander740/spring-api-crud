fda22405ae97056074637bab223cb3ccc19faa8f
Wanderson Ramos <wandersonramos740@gmail.com>
2023-10-05 23:17:13 -0300

    refactor: delete bean initDatabase

--- src/main/java/com/wanderson/blog/BlogApplication.java
+++ src/main/java/com/wanderson/blog/BlogApplication.java
@@ -19,35 +19,4 @@ public class BlogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
-
-	@Bean
-	CommandLineRunner initDatabase(CategoryRepository categoryRepository) {
-		return args -> {
-			categoryRepository.deleteAll();
-
-			Category category = new Category();
-			category.setTitle("Sports");
-
-			Category category1 = new Category();
-			category1.setTitle("news");
-
-			News c = new News();
-			c.setTitle("Teste um");
-			c.setText("adfasdfsadfsadfsadf");
-			c.setCategory(category);
-
-			category.getNews().add(c);
-			
-			News c1 = new News();
-			c1.setTitle("Teste dois");
-			c1.setText("adfasdfsadfsafasdfaadfsadf");
-			c1.setCategory(category1);
-
-			category1.getNews().add(c1);
-
-			categoryRepository.save(category);
-
-			categoryRepository.save(category1);
-		};
-	}
 }
