58b51dacb205b4601d3ffa2669f05c41ff8b7e3d
Wanderson Ramos <wandersonramos740@gmail.com>
2023-10-07 15:51:13 -0300

    fix: cors error

--- src/main/java/com/wanderson/blog/controller/NewsController.java
+++ src/main/java/com/wanderson/blog/controller/NewsController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Validated
 @RestController
 @RequestMapping("/api/news")
+@CrossOrigin(origins = "*")
 public class NewsController {
 
     private final NewsService newsService;
