2d4070c484d2b6347c8cced36c16ec5b2376c606
Wanderson Ramos <wandersonramos740@gmail.com>
2023-10-09 13:50:16 -0300

    fix: cors error category

--- src/main/java/com/wanderson/blog/controller/CategoryController.java
+++ src/main/java/com/wanderson/blog/controller/CategoryController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ import jakarta.validation.constraints.Positive;
 @Validated
 @RestController
 @RequestMapping("/api/category")
+@CrossOrigin(origins = "*")
 public class CategoryController {
 
     private final CategoryService categoryService;
